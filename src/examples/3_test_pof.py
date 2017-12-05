8bf8085aea051b9c852c96c02037bb54b5ed66dc
mframpton <mjeframpton@gmail.com>
2017-12-05 11:21:05 +0000

    1st commit.

---
+++ src/examples/3_test_pof.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pof.pof import Family
+from pof.pof import Pof
+
+
+'''
+Create some families and genotypes, then check which families pass.    
+'''
+family_1 = Family("1","A3N2",["1_1","1_2","1_3"],["1_4","1_5"],A_n_min=3,N_n_min=2,AN_p_diff_min=0.5)
+family_1.print_info()
+family_2 = Family("2","A4N1",["2_10","2_11","2_12","2_13","2_14"],["2_15"],A_n_min=4,N_n_min=1,A_p_min=1.0)
+family_2.print_info()
+family_l = [family_1,family_2]
+geno_s = pd.Series(data=["1","1","1","1","0","1","1","1","1","1","1"],
+                index=["1_1","1_2","1_3","1_4","1_5","2_10","2_11","2_12","2_13","2_14","2_15"])
+pof = Pof(family_l)
+family_pass_l = pof.get_family_pass_name_l(geno_s)
+print family_pass_l
