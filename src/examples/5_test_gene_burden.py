7ef0aacd12d02bb39afe77d446aed0aaf4c92f68
mframpton <mjeframpton@gmail.com>
2017-12-07 18:15:23 +0000

    Gene burden class and code for testing it.

--- src/examples/5_test_gene_burden.py
+++ src/examples/5_test_gene_burden.py
@@ -1,38 +1,91 @@
 import pandas as pd
-import sys
 import numpy as np
+import sys
 import os
-import argparse
-import burden as b
-import general as g
-import log as l
-import parent_args as pa
+from gene_burden.gene_burden import CMC
 
 
-'''Read in arguments, set input directories and create log file.'''
-parser = argparse.ArgumentParser(description="Filter variants and prepare files for gene burden tests.", parents=[pa.get_parent_parser()])
-args = parser.parse_args()
-pa.add_path_args(args)
-frq_grp_name_l = args.frq_grp_names.split(",")
-frq_grp_range_ll = [[float(frq) for frq in frq_range.split("-")] for frq_range in args.frq_grp_ranges.split(",")]
+'''Read in the genotype file.'''
+data_dir = os.path.abspath(os.path.join("..","..","data","gene_burden"))
+#print data_dir
+geno_df = pd.read_csv(os.path.join(data_dir,".".join(["16","3","5","csv"])), dtype=str)
+#print geno_df.ix[geno_df["SYMBOL"]=="NOD2",["Gene","Feature","SYMBOL"]]
+#sys.exit()
 
-'''Get the unrelated geno_df'''
-[log_stream, geno_df, sample_dict, caco_s] = b.get_unrelated_geno_df(args)
-#print geno_df.head()
+'''Get the samples.'''
+sample_l = geno_df.columns.tolist()
+sample_l = sample_l[sample_l.index("hardy_wein_p_all")+1:]
+#print sample_l
+#print len(sample_l)
+sample_dict = {}
+sample_dict["all"] = sample_l
+#Need to work out which samples are cases and which controls.
+print sample_l
+case_l = filter(lambda x: x.startswith(("Levine_","BGI_Feb2017","BGI_Sept2015")), sample_l)
+control_l = filter(lambda x: x not in case_l, sample_l)
 
-'''Collapsing: within each gene-transcript, collapse the rare variants.'''
-l.log(log_stream, "Collapsing variants...")
-l.log(log_stream, str(frq_grp_name_l))
-l.log(log_stream, str(frq_grp_range_ll))
-drop_col_l = filter(lambda x: x not in ["CHROM","SYMBOL","Gene","Feature","BroadAJcontrols_ALT","gnomad_AF_NFE","EXAC_AF_NFE"] + sample_dict["all"], geno_df.columns.tolist())
-geno_df.drop(drop_col_l, axis=1, inplace=True)
-geno_df = b.collapse_vars_in_frq_ranges(geno_df=geno_df, sample_l=sample_dict["all"], frq_grp_name_l=frq_grp_name_l, frq_grp_range_ll=frq_grp_range_ll)
-#print geno_df.head()
-geno_df.reset_index(inplace=True)
-geno_df = geno_df.append(caco_s)
-geno_df = geno_df.reindex(columns=["SYMBOL","CHROM","Gene","Feature","Collapsed_var","n"] + sample_dict["all"])
-#print geno_df.shape
-[chunk_num, chunk_denom] = [i for i in args.chunk.split("/")]
-geno_df.to_csv(os.path.join(args.data_dir,".".join([str(args.chr),chunk_num,chunk_denom,"collapsed.csv.gz"])), compression="gzip", index=False)
-l.log(log_stream, "FINISHED!")
+'''Make the gene column '''
+geno_df[["CHROM","POS","REF","ALT"]] = geno_df["VARIANT_ID"].str.extract('(?P<CHROM>.*?)_(?P<POS>.*?)_(?P<REF>.*?)_(?P<ALT>.*)', expand=False)
+geno_df["gene_transcript"] = geno_df[["CHROM","Gene","Feature","SYMBOL"]].apply(lambda row_s: "_".join(row_s.tolist()), axis=1)
+geno_df.drop(["CHROM","POS","REF","ALT"], inplace=True, axis=1)
 
+'''Do the gene burden.'''
+cmc = CMC(sample_dict=sample_dict, gene_col="gene_transcript", frq_col_l=["BroadAJcontrols_ALT","gnomad_AF_NFE","EXAC_NFE"])
+geno_collapsed_df = cmc.get_geno_collapsed_df(geno_df)
+print geno_collapsed_df
+print len(geno_collapsed_df.index)
+
+
+#from scipy.stats import chisqprob
+#import statsmodels.api as sm
+#from statsmodels.discrete.discrete_model import Logit
+import statsmodels.discrete.discrete_model as sm
+from scipy import stats
+stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
+
+#def likelihood_ratio(llmin, llmax):
+    #return(2*(llmax-llmin))
+
+
+#Try NOD2
+print geno_collapsed_df.ix["16_ENSG00000167207_ENST00000300589_NOD2"]
+print geno_collapsed_df.index
+
+#print geno_collapsed_df[]
+#print geno_collapsed_df[]
+
+print [1 if sample in case_l else 0 for sample in sample_l]
+y = np.array([1 if sample in case_l else 0 for sample in sample_l])
+print y.shape
+X = np.transpose(geno_collapsed_df.ix["16_ENSG00000167207_ENST00000300589_NOD2",sample_l].values)
+print X
+print X.shape
+
+logit_model=sm.Logit(y,X)
+result=logit_model.fit()
+print(result.summary())
+print result.params
+print result.llr_pvalue
+
+#LR = likelihood_ratio(L1,L2)
+#p = chisqprob(LR, 1) # L2 has 1 DoF more than L1
+#print 'p: %.30f' % p 
+
+
+#def do_multivariate_test(geno_collapsed_):
+
+#result_df = geno_collapsed_df.apply(do_multivariate_test, axis=1)
+
+#geno_collapsed_df[]
+
+def do_multivariate_test(geno_collapsed_gene_df, sample_l, y):
+    
+    print geno_collapsed_gene_df.name
+    X = np.transpose(geno_collapsed_gene_df[sample_l].values)
+    print np.all(X==0)
+    logit_model=sm.Logit(y,X)
+    result=logit_model.fit()
+    return result.llr_pvalue
+
+result_s = geno_collapsed_df.groupby(level=['gene_transcript']).apply(do_multivariate_test, sample_l=sample_l, y=y)
+print result_s
---
+++ src/gene_burden/gene_burden.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+
+class CMC(object):
+    
+    def __init__(self, sample_dict, gene_col, frq_col_l, frq_grp_name_l=["rare","mod_rare"], frq_grp_range_ll=[[0.0,0.01],[0.01,0.05]]):
+        
+        self.sample_dict = sample_dict
+        self.gene_col = gene_col
+        self.frq_col_l = frq_col_l
+        self.frq_grp_name_l=frq_grp_name_l
+        self.frq_grp_range_ll=frq_grp_range_ll
+    
+       
+    def get_geno_collapsed_df(self, geno_df):
+        
+        geno_df[self.frq_col_l] = geno_df[self.frq_col_l].apply(pd.to_numeric, axis=1)
+        geno_collapsed_df = self.collapse_vars_in_frq_ranges(geno_df=geno_df)
+        return geno_collapsed_df
+    
+        
+    def collapse_vars_in_frq_ranges(self, geno_df):
+
+        geno_df["collapsed_var"] = geno_df.apply(axis=1, func=self.assign_collapse_group)
+        collapse_group_n_df = geno_df.groupby(self.gene_col)["collapsed_var"].value_counts().to_frame("n")
+        geno_collapsed_df = geno_df.groupby([self.gene_col] + ["collapsed_var"]).apply(func=self.collapse_vars_in_samples)
+        geno_collapsed_df = geno_collapsed_df.merge(collapse_group_n_df, left_index=True, right_index=True)
+        return geno_collapsed_df
+
+
+    def assign_collapse_group(self, geno_s):
+        '''Return each variant's collapse group. If the variant is not within a collapse group, just return the variant ID.'''
+        
+        var_frq = filter(lambda x: np.isnan(x) == False, geno_s.ix[self.frq_col_l].tolist()+[0.0])[0]
+        for i in xrange(len(self.frq_grp_name_l)):
+            if var_frq >= self.frq_grp_range_ll[i][0] and var_frq <= self.frq_grp_range_ll[i][1]:
+                return self.frq_grp_name_l[i]
+        return geno_s.name
+
+
+    def collapse_vars_in_samples(self, gene_trans_group_df):   
+        
+        return gene_trans_group_df.ix[:,self.sample_dict["all"]].apply(axis=0, func=self.collapse_vars_in_1_sample)
+
+
+    def collapse_vars_in_1_sample(self, sample_geno_s):
+        
+        geno_count_s = sample_geno_s.value_counts()
+        if "1" in geno_count_s.index and geno_count_s.ix["1"] > 0:
+                return 1
+        elif "2" in geno_count_s.index and geno_count_s.ix["2"] > 0:
+                return 1
+        else:
+                return 0
