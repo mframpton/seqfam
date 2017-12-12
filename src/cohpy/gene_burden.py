b09e0ed159e78a1f6a52f4c5cc362db30a1e815c
mframpton <mjeframpton@gmail.com>
2017-12-12 16:57:47 +0000

    Population frequency category input is now a dictionary and np.digitize used for binning population frequencies.

--- src/cohpy/gene_burden.py
+++ src/cohpy/gene_burden.py
@@ -5,18 +5,21 @@ from scipy import stats
 stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
 import sys
 from misc import Logger
+import operator
+from collections import OrderedDict
 
 
 class CMC(object):
     
-    def __init__(self, sample_dict, gene_col, frq_col_l, frq_grp_name_l=["rare","mod_rare"], frq_grp_range_ll=[[0.0,0.01],[0.01,0.05]]):
+    def __init__(self, sample_dict, gene_col, frq_col_l, pop_frq_grp_dict={"rare":0.01,"mod_rare":0.05}):
     
         self.logger = Logger()    
         self.sample_dict = sample_dict
         self.gene_col = gene_col
         self.frq_col_l = frq_col_l
-        self.frq_grp_name_l=frq_grp_name_l
-        self.frq_grp_range_ll=frq_grp_range_ll
+        pop_frq_grp_dict = OrderedDict(sorted(pop_frq_grp_dict.items(),key=operator.itemgetter(1)))
+        self.pop_frq_grp_name_l = pop_frq_grp_dict.keys()
+        self.pop_frq_bin_arr = np.array(pop_frq_grp_dict.values())
     
     
     def do_multivariate_tests(self, geno_df, covar_df):
@@ -38,6 +41,7 @@ class CMC(object):
     
     
     def get_geno_collapsed_df(self, geno_df):
+        '''Main method for making new dataframe with collapsed genotypes.'''
         
         geno_df[self.frq_col_l] = geno_df[self.frq_col_l].apply(pd.to_numeric, axis=1)
         geno_df[self.sample_dict["all"]] = geno_df[self.sample_dict["all"]].apply(pd.to_numeric, errors='coerce', downcast='integer', axis=1)
@@ -49,7 +53,11 @@ class CMC(object):
     def collapse_vars_in_frq_ranges(self, geno_df):
 
         self.logger.log("Assign variants to a population frequency group.")
-        geno_df["collapsed_var"] = geno_df.apply(axis=1, func=self.assign_collapse_group)
+        pop_frq_s = geno_df.apply(lambda row_s: filter(lambda x: np.isnan(x) == False, row_s.ix[self.frq_col_l].tolist()+[0.0])[0],axis=1)
+        pop_frq_idx_arr = np.digitize(pop_frq_s.values,self.pop_frq_bin_arr)
+        geno_df = geno_df.join(pd.Series(data=pop_frq_idx_arr, index=pop_frq_s.index, name="pop_frq_cat_idx"))
+        geno_df["collapsed_var"] = geno_df.apply(lambda row_s: row_s.name if row_s["pop_frq_cat_idx"] == len(self.pop_frq_grp_name_l) else self.pop_frq_grp_name_l[row_s["pop_frq_cat_idx"]], axis=1)
+        geno_df.drop("pop_frq_cat_idx", inplace=True, axis=1)
         self.logger.log("For each gene, count the # of variants in each population frequency category.")
         collapse_group_n_df = geno_df.groupby(self.gene_col)["collapsed_var"].value_counts().to_frame("n")
         self.logger.log("In each gene, aggregate sample genotypes by variant population frequency group.")
@@ -58,16 +66,6 @@ class CMC(object):
         #sys.exit()
         geno_collapsed_df = geno_collapsed_df.merge(collapse_group_n_df, left_index=True, right_index=True)
         return geno_collapsed_df
-
-
-    def assign_collapse_group(self, geno_s):
-        '''Return each variant's collapse group. If the variant is not within a collapse group, just return the variant ID.'''
-        
-        var_frq = filter(lambda x: np.isnan(x) == False, geno_s.ix[self.frq_col_l].tolist()+[0.0])[0]
-        for i in xrange(len(self.frq_grp_name_l)):
-            if var_frq >= self.frq_grp_range_ll[i][0] and var_frq <= self.frq_grp_range_ll[i][1]:
-                return self.frq_grp_name_l[i]
-        return geno_s.name
 
     
     def collapse_vars_in_samples(self, gene_pop_frq_group_df):
@@ -111,11 +109,9 @@ class CMC(object):
     def get_collapsed_var_count_df(self, geno_collapsed_df):
         
         collapsed_var_count_df = geno_collapsed_df['n'].reset_index()
-        collapsed_var_count_df["collapsed_var"] = collapsed_var_count_df["collapsed_var"].apply(lambda x: x if x in self.frq_grp_name_l else "other")
+        collapsed_var_count_df["collapsed_var"] = collapsed_var_count_df["collapsed_var"].apply(lambda x: x if x in self.pop_frq_grp_name_l else "uncollapsed")
         collapsed_var_count_df = pd.pivot_table(data=collapsed_var_count_df, values='n', index=self.gene_col, columns='collapsed_var')
-        collapsed_var_count_col_l = collapsed_var_count_df.columns.tolist()
-        collapsed_var_count_col_l.sort(key = lambda x: len(self.frq_grp_name_l) if x=="other" else self.frq_grp_name_l.index(x))
-        collapsed_var_count_df = collapsed_var_count_df.reindex(columns=collapsed_var_count_col_l)
+        collapsed_var_count_df = collapsed_var_count_df.reindex(columns=self.pop_frq_grp_name_l+["uncollapsed"])
         collapsed_var_count_df.fillna(0, inplace=True)
         collapsed_var_count_df = collapsed_var_count_df.apply(pd.to_numeric, downcast='integer', axis=1)
         return collapsed_var_count_df
