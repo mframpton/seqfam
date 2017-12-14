f6c0cae7e195c6bd9fb6847a27a06bf691c430d2
mframpton <mjeframpton@gmail.com>
2017-12-14 17:50:14 +0000

    Lined for testing family tree generation added.

--- src/examples/1_test_gene_drop.py
+++ src/examples/1_test_gene_drop.py
@@ -8,6 +8,8 @@ cohort_tsv = os.path.join(data_dir,"cohort.tsv")
 cohort = Cohort(cohort_tsv)
 #print help(cohort)
 #print help(cohort.gene_drop)
+#cohort.fam_tree_l[0].log_all_info()
+#sys.exit()
 
 p = cohort.gene_drop(0.025, 0.025, 1000)
 p = cohort.gene_drop(0.025, 0.03, 1000)
