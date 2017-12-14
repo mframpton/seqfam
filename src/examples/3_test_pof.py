62e5b6371a359d4fb0ad2344cfd7864299239f8e
mframpton <mjeframpton@gmail.com>
2017-12-14 17:50:56 +0000

    Tidying up.

--- src/examples/3_test_pof.py
+++ src/examples/3_test_pof.py
@@ -10,8 +10,8 @@ family_1.log_info()
 family_2 = Family("2","A4N1",["2_10","2_11","2_12","2_13","2_14"],["2_15"],A_n_min=4,N_n_min=1,A_p_min=1.0)
 family_2.log_info()
 family_l = [family_1,family_2]
-geno_s = pd.Series(data=["1","1","1","1","0","1","1","1","1","1","1"],
-                index=["1_1","1_2","1_3","1_4","1_5","2_10","2_11","2_12","2_13","2_14","2_15"])
+variant_genotypes_s = pd.Series(data=["1","1","1","1","0","1","1","1","1","1","1"],
+                                index=["1_1","1_2","1_3","1_4","1_5","2_10","2_11","2_12","2_13","2_14","2_15"])
 pof = Pof(family_l)
-family_pass_l = pof.get_family_pass_name_l(geno_s)
+family_pass_l = pof.get_family_pass_name_l(variant_genotypes_s)
 print family_pass_l
