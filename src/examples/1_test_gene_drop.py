c17c260ec0f36c24df2bc13aa6e1ff779e830259
mframpton <mjeframpton@gmail.com>
2018-01-18 11:59:18 +0000

    Updated to use new version of cohort.gene_drop function.

--- src/examples/1_test_gene_drop.py
+++ src/examples/1_test_gene_drop.py
@@ -11,7 +11,13 @@ cohort = Cohort(cohort_tsv)
 #cohort.fam_tree_l[0].log_all_info()
 #sys.exit()
 
-p = cohort.gene_drop(0.025, 0.025, 1000)
-p = cohort.gene_drop(0.025, 0.03, 1000)
-p = cohort.gene_drop(0.025, 0.035, 1000)
-p = cohort.gene_drop(0.025, 0.04, 1000)
+#Get a list of genotyped individuals.
+cohort_sample_l = cohort.get_all_sample_l()
+sample_genotyped_l = list(filter(lambda sample: sample.find("p") == -1,cohort_sample_l))
+#sample_genotyped_l = cohort.get_all_sample_l()
+
+#Do gene dropping.
+p = cohort.gene_drop(0.025, 0.025, sample_genotyped_l, 1000)
+p = cohort.gene_drop(0.025, 0.03, sample_genotyped_l, 1000)
+p = cohort.gene_drop(0.025, 0.035, sample_genotyped_l, 1000)
+p = cohort.gene_drop(0.025, 0.04, sample_genotyped_l, 1000)
