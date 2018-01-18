1172e5ec6d5c36523c85087c0358dce063dfb682
mframpton <mjeframpton@gmail.com>
2018-01-18 11:58:33 +0000

    Which samples are genotyped for variant now specified in argument of cohort.gene_drop function.

--- src/cohpy/gene_drop.py
+++ src/cohpy/gene_drop.py
@@ -5,18 +5,18 @@ import time
 from cohpy.misc import Logger
 import sys
 from natsort import natsorted
+from itertools import groupby
 
 
 class Node(object):
     '''Represents an individual (node) in a family tree.'''
 
-    def __init__(self, id, parent_l, spouse, children_l, sequenced_b):
+    def __init__(self, id, parent_l, spouse, children_l):
         self.id = id
         self.parent_l = parent_l
         self.spouse = spouse
         self.children_l = children_l
         self.genotype = None
-        self.sequenced_b = sequenced_b
     
     
     def get_summary_str(self):
@@ -29,8 +29,7 @@ class Node(object):
                          "Parents: {0}".format(",".join([parent_node.id for parent_node in self.parent_l])),
                          "Spouse: {0}".format(self.spouse.id if self.spouse != None else ""),
                          "Children: {0}".format(",".join([child_node.id for child_node in self.children_l])),
-                         "Genotype: {0}".format(self.genotype),
-                         "Sequenced: {0}".format(self.sequenced_b)]
+                         "Genotype: {0}".format(self.genotype)]
         summary_str = "; ".join(summary_str_l)
         return summary_str
 
@@ -48,7 +47,7 @@ class NodeGenerator(object):
             node_l (list of Nodes): the Node objects represent the individuals in the pedigree DataFrame.'''
 
         #Create the nodes.
-        name_node_dict = OrderedDict(ped_df.apply(lambda row_s: (row_s["PERSON"], Node(row_s["PERSON"],[],None,[],row_s["SEQUENCED"])), axis=1).tolist())
+        name_node_dict = OrderedDict(ped_df.apply(lambda row_s: (row_s["PERSON"], Node(row_s["PERSON"],[],None,[])), axis=1).tolist())
         #Now set the relationships between them.
         ped_df.apply(self.set_relationships, name_node_dict=name_node_dict, axis=1)
         node_l = name_node_dict.values()
@@ -81,17 +80,16 @@ class FamilyTree(object):
         self.id = id
         self.node_l = node_l
         self.founder_l = list(filter(lambda x: not x.parent_l, self.node_l))
-        self.sequenced_n = len([node for node in node_l if node.sequenced_b == True])
 
 
-    def gene_drop(self, pop_af):
+    def gene_drop(self, pop_af, ind_gtyped_l):
         '''Main method for performing gene dropping for 1 variant in this family tree.
         
         Args:
             pop_af (float): population allele frequency of the variant.
-            
+            ind_gtyped_l (list of strs): IDs of individuals in this family who have a genotype for the variant. 
         Returns:
-            carrier_allele_count (int): the number of (sequenced) carriers in the family after dropping the gene.'''
+            carrier_allele_count (int): the number of (genotyped) carriers in the family after dropping the gene.'''
         
         #Set founder genotypes using the population allele frq & select 1 per spousal pair from which to start gene dropping.
         founder_no_spouse_l = []
@@ -103,8 +101,7 @@ class FamilyTree(object):
         for founder_node in founder_no_spouse_l:
             self.gene_drop_dfs(founder_node)
         #Get the carrier allele count and reset the genotypes.
-        #self.log_all_genotypes(sequenced_only_b=True)
-        carrier_allele_count = sum((node.genotype for node in self.node_l if node.sequenced_b == True))
+        carrier_allele_count = sum((node.genotype for node in self.node_l if node.id in ind_gtyped_l))
         for node in self.node_l:
             node.genotype = None
         return carrier_allele_count
@@ -150,14 +147,10 @@ class FamilyTree(object):
             node.genotype = get_allele_passed_to_offspring(node.parent_l[0].genotype) + get_allele_passed_to_offspring(node.parent_l[1].genotype)
 
 
-    def log_all_genotypes(self, sequenced_only_b=False):
-        '''Log all of the node genotypes.
-        
-        Args:
-            sequenced_only_b (boolean): whether to log the genotypes for only the sequenced individuals, or all individuals, defaults to False.'''
+    def log_all_genotypes(self):
+        '''Log all of the node genotypes.'''
 
-        node_to_log_l = self.node_l if sequenced_only_b == False else filter(lambda node: node.sequenced_b == True,self.node_l)
-        self.logger.log(" ".join(["FAMILY {}:".format(self.id)] + ["{0}:{1}".format(node.id,node.genotype) for node in node_to_log_l]))
+        self.logger.log(" ".join(["FAMILY {}:".format(self.id)] + ["{0}:{1}".format(node.id,node.genotype) for node in self.node_l]))
 
 
     def log_all_info(self):
@@ -175,7 +168,6 @@ class Cohort(object):
         self.logger = Logger()
         self.node_generator = NodeGenerator()
         cohort_df = pd.read_csv(cohort_tsv, sep="\t", dtype=str)
-        cohort_df["SEQUENCED"] = cohort_df["SEQUENCED"].apply(lambda x: eval(x))
         self.logger.log("Making family tree objects...")
         self.fam_tree_l = cohort_df.groupby("FAMILY").apply(self.make_fam_tree).tolist()
         self.fam_tree_l = natsorted(self.fam_tree_l, key=lambda fam_tree: fam_tree.id)
@@ -195,28 +187,43 @@ class Cohort(object):
         return FamilyTree(self.logger, family_id, node_l)
     
     
-    def gene_drop(self, pop_af, cohort_af, gene_drop_n):
+    def get_all_sample_l(self):
+        '''Get a list of all of the samples in the cohort.
+        
+        Returns:
+            all_sample_l (list): list of sample IDs of the form <FAMILY_ID>_<INDIVIDUAL_ID>.'''
+        
+        all_sample_l = ["{0}_{1}".format(fam_tree.id,node.id) for fam_tree in self.fam_tree_l for node in fam_tree.node_l]
+        return all_sample_l
+    
+    
+    def gene_drop(self, pop_af, cohort_af, sample_genotyped_l, gene_drop_n):
         '''Perform gene dropping across the cohort and return the proportion of iterations in which the simulated allele frequency is less than or equal to the cohort frequency.
         
         Args:
             pop_af (float): population allele frequency.
             cohort_af (float): cohort allele frequency.
+            sample_genotyped (list of strs): the list of samples genotyped for this variant from which cohort af was calculated.
             gene_drop_n (int): number of iterations to perform.
         
         Returns:
             cohort_enriched_p (float): proportion of iterations in which the simulated allele frequency is less than or equal to the cohort frequency.'''
         
-        self.logger.log("Start gene drop with pop_af={0}, cohort_af={1} & gene_drop_n={2}.".format(pop_af,cohort_af,gene_drop_n))
+        ind_gtyped_grps = groupby(sample_genotyped_l, lambda x: x.split("_")[0])
+        fam_ind_gtyped_dict = OrderedDict([(fam,[id.split("_")[1] for id in list(ind_gtyped_grp)]) for fam, ind_gtyped_grp in ind_gtyped_grps])
+        self.logger.log("# families with >=1 genotyped sample: {0}".format(len(fam_ind_gtyped_dict)))
+        self.logger.log("Start gene drop with pop_af={0}, cohort_af={1}, # genotype calls={2} & gene_drop_n={3}.".format(pop_af,cohort_af,len(sample_genotyped_l),gene_drop_n))
+        
         t0 = time.time()
-        total_allele_count = sum((fam_tree.sequenced_n*2 for fam_tree in self.fam_tree_l))
+        total_allele_count = len(sample_genotyped_l)*2
         lt_thresh_n = 0
         for n in range(gene_drop_n):
-            carrier_allele_count = sum(fam_tree.gene_drop(pop_af) for fam_tree in self.fam_tree_l)
+            carrier_allele_count = sum(fam_tree.gene_drop(pop_af,fam_ind_gtyped_dict[fam_tree.id]) for fam_tree in self.fam_tree_l if fam_tree.id in fam_ind_gtyped_dict)
             gene_drop_af = carrier_allele_count/float(total_allele_count)
             if cohort_af <= gene_drop_af:
                 lt_thresh_n += 1        
         cohort_enriched_p = lt_thresh_n/float(gene_drop_n)
         self.logger.log("p-value={0}".format(cohort_enriched_p))
         t1 = time.time()
-        self.logger.log("Processing time: {0:.2f} secs".format(t1-t0))
+        self.logger.log("Processing time: {0:.2f} secs\n".format(t1-t0))
         return cohort_enriched_p
