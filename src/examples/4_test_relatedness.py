8c590175916d64e2523d9a258471d7fb99d2ab05
mframpton <mjeframpton@gmail.com>
2018-01-19 13:14:54 +0000

    King between families file no longer used because too big to include in package.

--- src/examples/4_test_relatedness.py
+++ src/examples/4_test_relatedness.py
@@ -3,21 +3,17 @@ from cohpy.relatedness import Relatedness
 import pandas as pd
 
 '''Create the relatedness object.'''
-bf_file = os.path.abspath(os.path.join("..","..","data","relatedness",".".join(["all","rel","kinship","ibs0"])))
-print(bf_file)
-wf_file = os.path.abspath(os.path.join("..","..","data","relatedness",".".join(["all","rel","kinship","ibs"])))
-print(wf_file)
+wf_file = os.path.abspath(os.path.join("..","..","data","relatedness",".".join(["king","kinship","ibs"])))
 cohort_tsv = os.path.abspath(os.path.join("..","..","data",".".join(["cohort","tsv"])))
-relatedness = Relatedness(bf_file,wf_file,cohort_tsv)
+relatedness = Relatedness(bf_file=None,wf_file,cohort_tsv)
 
 '''Within-family duplicates'''
 wf_duplicate_l = relatedness.find_duplicates(bf_b=False)
 print(wf_duplicate_l)
-'''Between-family duplicates...'''
-bf_duplicate_l = relatedness.find_duplicates(bf_b=True)
-print(bf_duplicate_l)
+'''Between-family duplicates... (Uncomment if you have a bf_file).'''
+#bf_duplicate_l = relatedness.find_duplicates(bf_b=True)
+#print(bf_duplicate_l)
 
 '''Expected versus observed within-family relationships.'''
 exp_obs_df = relatedness.get_exp_obs_df()
-#print exp_obs_df
 print(exp_obs_df.ix[(exp_obs_df["EXP_REL"]!=exp_obs_df["OBS_REL"]) & (pd.notnull(exp_obs_df["Kinship"])),:])
