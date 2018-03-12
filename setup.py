97b9e7e835fc1c2462a42df7b5111c5146c7c1c4
mframpton <mjeframpton@gmail.com>
2018-03-12 12:49:44 +0000

    1st commit of setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SeqFam',
+    version='0.1',
+    packages=['seqfam',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    author="Matthew Frampton",
+    author_email="mjeframpton@gmail.com",
+    description="The seqfam package is primarily designed for analysing next generation sequencing DNA data from families with pedigree information in order to identify rare variants that are potentially causal of a disease/trait.",
+)
