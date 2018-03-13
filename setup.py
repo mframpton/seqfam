2f4a5e79da9d5f27eebc02ea697e71bc2eafbefd
mframpton <mjeframpton@gmail.com>
2018-03-13 10:42:57 +0000

    Package name changed and URL added.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='SeqFam',
+    name='Seqfam',
     version='0.1',
     packages=['seqfam',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
@@ -9,4 +9,5 @@ setup(
     author="Matthew Frampton",
     author_email="mjeframpton@gmail.com",
     description="The seqfam package is primarily designed for analysing next generation sequencing DNA data from families with pedigree information in order to identify rare variants that are potentially causal of a disease/trait.",
+    url='https://github.com/mframpton/seqfam',
 )
