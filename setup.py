3405c8c920935d5c8b54c5ca13dfcbc700035aed
mframpton <mjeframpton@gmail.com>
2018-03-13 11:46:56 +0000

    Requirements.

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(
     author_email="mjeframpton@gmail.com",
     description="The seqfam package is primarily designed for analysing next generation sequencing DNA data from families with pedigree information in order to identify rare variants that are potentially causal of a disease/trait.",
     url='https://github.com/mframpton/seqfam',
+    install_requires=['pandas==0.20.3','scipy==0.19.1','natsort==5.1.1','numpy==1.13.3','setuptools==38.4.0','statsmodels==0.8.0'],
 )
